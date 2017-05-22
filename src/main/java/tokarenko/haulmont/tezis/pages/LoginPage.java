8c409fca1c93e123194b08ef5b7d3c4ce67b92f9
MTokarenko <mike.tokarenko@gmail.com>
2017-05-22 22:14:52 +0400

    global settings refactoring

--- src/main/java/tokarenko/haulmont/tezis/pages/LoginPage.java
+++ src/main/java/tokarenko/haulmont/tezis/pages/LoginPage.java
@@ -3,12 +3,10 @@ package tokarenko.haulmont.tezis.pages;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
-import org.openqa.selenium.support.ui.WebDriverWait;
+
 import tokarenko.AbstractPage;
 
-/**
- * Created by Mikhail on 20.05.2017.
- */
+
 public class LoginPage extends AbstractPage {
 
     @FindBy(id = "s")
